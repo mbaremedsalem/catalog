4ea758365ec925ca5069f9dd9e178ad0d1946ae6
mbaremedsalem <mbaremedsalemmbare@gmail.com>
2024-07-16 17:46:02 +0200

    initial commit

---
+++ bankend/core/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from .views import *
+
+app_name = "userauths"
+urlpatterns = [
+    path("sign-up",RegisterView,name="sign-up"),
+    path("sign-in/", LoginView, name="sign-in"),
+    path("sign-out/", logoutView, name="sign-out"),
+    #------ api rest framwork -----
+    path("login/", MyLoginView.as_view(), name='api_login'),
+    path('register/', MyRegisterView.as_view(), name='register'),
+    path('logout/', MyLogoutView.as_view(), name='logout'),
+
+
+    # ----- account ------
+    # path("dashboard/", dashboard, name="dashboard"),
+    path("", account, name="account"),
+    # path("kyc-reg/", kyc_registration, name="kyc-reg"),
+]
